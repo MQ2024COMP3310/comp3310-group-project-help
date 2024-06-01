c420387302ce92d6e12f414875f4f22944f5dfa2
LawsonDuong <luan.duong1@students.mq.edu.au>
2024-06-01 23:08:26 +1000

    update

--- project/__init__.py
+++ project/__init__.py
@@ -1,23 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 import os
 from pathlib import Path
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-
     app.config['SECRET_KEY'] = 'secret-key-do-not-reveal'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///photos.db'
+    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
     CWD = Path(os.path.dirname(__file__))
     app.config['UPLOAD_DIR'] = CWD / "uploads"
 
     db.init_app(app)
+    jwt.init_app(app)
 
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
-
+    app.register_blueprint(auth_blueprint)
+    
     return app
--- project/models.py
+++ project/models.py
@@ -1,5 +1,11 @@
 from . import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
 class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
