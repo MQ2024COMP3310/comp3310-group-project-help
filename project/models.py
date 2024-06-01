bc837ff48b307262e7029f6644b6ecf2e1137eee
gavinyap28 <80371446+gavinyap28@users.noreply.github.com>
2024-06-01 20:49:03 +1000

    Update models.py

--- project/models.py
+++ project/models.py
@@ -17,4 +17,36 @@ class Photo(db.Model):
            'file'         : self.file,
            'desc'         : self.description,
        }
- 
+        
+ class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)  # Assuming user ID is stored as integer
+    name = db.Column(db.String(50), nullable=False)
+    photos = db.relationship('Photo', backref='album', lazy=True)  # Relationship to photos
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'name': self.name,
+            'photos': [photo.serialize for photo in self.photos]
+        }
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    photo_id = db.Column(db.Integer, db.ForeignKey('photo.id'), nullable=False)  # Foreign key to Photo
+    user_id = db.Column(db.Integer, nullable=False)  # Assuming user ID is stored as integer
+    content = db.Column(db.String(500), nullable=False)
+    photo = db.relationship('Photo', backref='comments', lazy=True)  # Relationship to photo
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'photo_id': self.photo_id,
+            'user_id': self.user_id,
+            'content': self.content,
+        }
